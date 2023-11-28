b9dbd6b2c47e4d58fea41cb11bc46924975ae24e
Replit user <>
2023-11-28 06:03:43 +0000

    connect to database and create user schema

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+//Import MongoDB library
+const mongoose = reuire('mongoose');
+const { Schema } = mongoose;
+
+// Connecting to the database
+mongoose.connect(process.env['MONGO_URI'])
+
+// Creating User Schema
+const UserSchema = new Schema({
+  username: String,
+});
+const User = mongoose.model("User", UserSchema);
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
